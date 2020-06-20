93c4c9f763fdf0fb26093ed74f2fabd960963cf5
MikiKru <michal_kruczkowski@o2.pl>
2020-06-20 10:55:03 +0200

    hibernate -d2 -v6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,33 @@ import config.HibernateConfiguration;
 import controller.HibernateController;
 import org.hibernate.Session;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         HibernateController hc = new HibernateController();
-        hc.addUser("test1", "test1", "test1@test.pl", "test1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("APLIKACJA BLOGA");
+        boolean isLogged = false;
+        while (true) {
+            if (!isLogged) {
+                System.out.println("Podaj login:");
+                String login = scanner.nextLine();
+                System.out.println("Podaj hasło:");
+                String password = scanner.nextLine();
+                    
+            } else {
+                System.out.println("Co chcesz zrobić?");
+                System.out.println("1. Dodaj posta \n2. Zmień tutuł post \n3. Usuń posta \nQ. Wyjdź");
+                String decision = scanner.nextLine();
+                if (decision.toUpperCase().equals("Q")){
+                    break;
+                }
+            }
+
+        }
+
+
+//        hc.addUser("test1", "test1", "test1@test.pl", "test1");
     }
 }
