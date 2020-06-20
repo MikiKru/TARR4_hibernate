3fd8a663c755c0605645db1bd42fe943190e721a
MikiKru <michal_kruczkowski@o2.pl>
2020-06-20 11:03:52 +0200

    hibernate -d2 -v7

--- src/main/java/controller/HibernateController.java
+++ src/main/java/controller/HibernateController.java
@@ -2,6 +2,7 @@ package controller;
 
 import config.HibernateConfiguration;
 import model.User;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -15,5 +16,19 @@ public class HibernateController {
         session.save(new User(name,lastName,email,password));
         transaction.commit();
         session.close();
+    }
+    public User loginUser(String login, String password){
+        Session session = HibernateConfiguration.getSessionFactory().openSession();
+        Transaction transaction =session.beginTransaction();
+
+        Query query = session.createQuery("SELECT u FROM User u WHERE u.login=:login AND u.password=:password");
+        query.setString("login", login);
+        query.setString("password", password);
+        query.setMaxResults(1);
+        User user = (User) query.uniqueResult();
+
+        transaction.commit();
+        session.close();
+        return user;
     }
 }
